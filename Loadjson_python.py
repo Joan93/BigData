aec91f1b61fc7c6c9135efd82528de234a85e2f6
Joan93 <jfloritmir@gmail.com>
2016-04-19 20:04:52 +0200

    Guardar en fichero funcionando

--- Loadjson_python.py
+++ Loadjson_python.py
@@ -5,19 +5,18 @@ from pprint import pprint
 with open('Data/data.json') as data_file:
     data = json.load(data_file)
 
-print data["updateTime"]
+#print data["updateTime"]
+time=data["updateTime"]
 
-print ('Uptade Time: %f' %data["updateTime"])
-
-
+print ('Uptade Time: %i' %time)
 
 #pprint(data)
-
 #print data["stations"]
-
+#station
 
 
 for i in xrange(10):
+    #print ('Uptade Time: %i' %print data["stations"][i]["id"])
     print data["stations"][i]["id"]
     print data["stations"][i]["altitude"]
     print data["stations"][i]["longitude"]
@@ -30,3 +29,10 @@ for i in xrange(10):
 
 #for data[""]
 
+f = open("Process_Data/data_python.txt", 'w+')
+
+for i in xrange(10):
+
+    f.write(data["stations"][i]["id"]+"\n")
+    f.write(data["stations"][i]["altitude"]+"\n")
+    f.write(data["stations"][i]["longitude"]+"\n")
