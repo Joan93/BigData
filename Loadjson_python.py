b02622a1aabd14ae50f7041d6b12a56cfec307b0
Joan93 <jfloritmir@gmail.com>
2016-04-19 19:39:17 +0200

    LoadJSON PYTHON

--- Loadjson_python.py
+++ Loadjson_python.py
@@ -7,6 +7,26 @@ with open('Data/data.json') as data_file:
 
 print data["updateTime"]
 
+print ('Uptade Time: %f' %data["updateTime"])
+
+
+
 #pprint(data)
 
-print data["stations"]
+#print data["stations"]
+
+
+
+for i in xrange(10):
+    print data["stations"][i]["id"]
+    print data["stations"][i]["altitude"]
+    print data["stations"][i]["longitude"]
+    print data["stations"][i]["latitude"]
+    print data["stations"][i]["bikes"]
+    print data["stations"][i]["slots"]
+    print data["stations"][i]["type"]
+    print data["stations"][i]["status"]
+
+
+#for data[""]
+
