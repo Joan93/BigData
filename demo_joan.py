f1a0bfc6579c7b74ec4c1c2acd699fd3d4d7bd75
Joan93 <jfloritmir@gmail.com>
2016-04-19 17:30:51 +0200

    CAMBIOS EN DEMO JOAN

--- demo_joan.py
+++ demo_joan.py
@@ -1,3 +1,68 @@
+# PARA EJECUTAR EL PROGRAMA en pycharm con pyspark
+
+import sys
+if ("exec" not in sys.argv):
+ #Autoexecute SDK
+ import os
+ os.system('/Users/JoanIgnasi/Documents/spark-1.3.0-bin-hadoop2.4/bin/pyspark demo_joan.py exec')
+
+
+
+
+
+
+else:
+
+   # sc is an existing SparkContext.
+    from pyspark import SparkContext
+    from pyspark.sql import SQLContext
+
+    sc=SparkContext()
+    sqlContext = SQLContext(sc)
+
+    # A JSON dataset is pointed to by path.
+    # The path can be either a single text file or a directory storing text files.
+    path = "Data/data.json"
+
+    # Create a SchemaRDD from the file(s) pointed to by path
+    data = sqlContext.jsonFile(path)
+
+    # The inferred schema can be visualized using the printSchema() method.
+    data.printSchema()
+    # root
+    # |-- stations: array (nullable = true)
+    # |    |-- element: struct (containsNull = true)
+    # |    |    |-- altitude: string (nullable = true)
+    # |    |    |-- bikes: string (nullable = true)
+    # |    |    |-- id: string (nullable = true)
+    # |    |    |-- latitude: string (nullable = true)
+    # |    |    |-- longitude: string (nullable = true)
+    # |    |    |-- nearbyStations: string (nullable = true)
+    # |    |    |-- slots: string (nullable = true)
+    # |    |    |-- status: string (nullable = true)
+    # |    |    |-- streetName: string (nullable = true)
+    # |    |    |-- streetNumber: string (nullable = true)
+    # |    |    |-- type: string (nullable = true)
+    # |-- updateTime: long (nullable = true)
+
+
+    '''
+    # Register this SchemaRDD as a table.
+    data.registerTempTable("data")
+
+    # SQL statements can be run by using the sql methods provided by sqlContext.
+    teenagers = sqlContext.sql("SELECT name FROM people WHERE age >= 13 AND age <= 19")
+    '''
+
+    # Alternatively, a SchemaRDD can be created for a JSON dataset represented by
+    # an RDD[String] storing one JSON object per string.
+    anotherPeopleRDD = sc.parallelize([
+      '{"name":"Yin","address":{"city":"Columbus","state":"Ohio"}}'])
+    anotherPeople = sqlContext.jsonRDD(anotherPeopleRDD)
+
+
+
+''''
 # sc is an existing SparkContext.
 from pyspark.sql import SQLContext
 sqlContext = SQLContext(sc)
@@ -25,3 +90,6 @@ teenagers = sqlContext.sql("SELECT name FROM people WHERE age >= 13 AND age <= 1
 anotherPeopleRDD = sc.parallelize([
   '{"name":"Yin","address":{"city":"Columbus","state":"Ohio"}}'])
 anotherPeople = sqlContext.jsonRDD(anotherPeopleRDD)
+
+
+'''''
