c4f601a90f02d596c17228c77a8bdce5a5e0ef1c
rodrigo.sampedro <ropnom5291@gmail.com>
2016-04-20 12:28:53 +0200

    add commets

--- ProcessData.py
+++ ProcessData.py
@@ -20,39 +20,40 @@ script_file = "ProcessData.py"
 data_folder = "Data/"
 data_process_folder = "Process_Data/RDD/"
 
+# Auto-run Pycharm/python to Spark
 import sys
+import os
 if ("exec" not in sys.argv):
- #Autoexecute SDK
- import os
+ #execute the script vias Spark
  os.system(spark_path+" "+script_file+" exec")
 
 else:
+    # **** Script in SPARK ****
 
-    # sc is an existing SparkContext.
     from pyspark import SparkContext
     from pyspark.sql import SQLContext
     import json
 
+    #Config Spark
     sc=SparkContext()
     sqlContext = SQLContext(sc)
 
+    #Load data
     path = data_folder+"data.json"
-
     data_raw = sc.textFile(path)
+
+
     # Parse JSON entries in dataset
     data = data_raw.map(lambda line: json.loads(line))
-
-    # Extract relevant fields in dataset -- category label and text content
+    # Extract relevant fields in dataset
     time = data.map(lambda line: (line['updateTime']))
     data_filter = data.flatMap(lambda line: line['stations'][:])\
         .map(lambda station: [station['id'],station['altitude'],station['latitude'],station['longitude'],station['bikes'],station['slots'],station['type'],station['status']])
-
+    #Union both dataset
     total =  time.union(data_filter).collect()
 
-    #for data[""]
-
+    #Write Compact file with filtered data
     f = open(data_process_folder+"data.txt", 'w+')
-
     line=""
     first= True
 
