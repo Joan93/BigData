3fb00bb2421572f5aa863b8a18b6f9710e0c1795
rodrigo.sampedro <ropnom5291@gmail.com>
2016-04-20 12:21:14 +0200

    ProcessData update. New standar of scripts

---
+++ ProcessData.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+#title           :ProcessData.py
+#description     :This script process json files from Barcelona Bicing, filtering and storage in a better form.
+#author          :Rodrigo
+#date            :2016-04-19
+#version         :0.1
+#usage           :python pyscript.py
+#notes           :
+#python_version  :2.7.6
+#requirements    :Spark 1.3
+#==============================================================================
+
+
+# UPC-EETAC MASTEAM 2015-2016 BIGDATA
+# Group former by Ana, Lucia, Joan and Rodrigo
+
+spark_path = "/home/rodrigo/Programe_Files_Linux/spark-1.3.0-bin-hadoop2.4/bin/spark-submit"
+script_file = "ProcessData.py"
+data_folder = "Data/"
+data_process_folder = "Process_Data/RDD/"
+
+import sys
+if ("exec" not in sys.argv):
+ #Autoexecute SDK
+ import os
+ os.system(spark_path+" "+script_file+" exec")
+
+else:
+
+    # sc is an existing SparkContext.
+    from pyspark import SparkContext
+    from pyspark.sql import SQLContext
+    import json
+
+    sc=SparkContext()
+    sqlContext = SQLContext(sc)
+
+    path = data_folder+"data.json"
+
+    data_raw = sc.textFile(path)
+    # Parse JSON entries in dataset
+    data = data_raw.map(lambda line: json.loads(line))
+
+    # Extract relevant fields in dataset -- category label and text content
+    time = data.map(lambda line: (line['updateTime']))
+    data_filter = data.flatMap(lambda line: line['stations'][:])\
+        .map(lambda station: [station['id'],station['altitude'],station['latitude'],station['longitude'],station['bikes'],station['slots'],station['type'],station['status']])
+
+    total =  time.union(data_filter).collect()
+
+    #for data[""]
+
+    f = open(data_process_folder+"data.txt", 'w+')
+
+    line=""
+    first= True
+
+    for a in total:
+        if(first):
+            line= str(a)
+            first=False
+        else:
+         line = str(a[0])+" "+str(a[1])+" "+str(a[2])+" "+str(a[3])+" "+str(a[4])+" "+str(a[5])+" "+str(a[6])+" "+str(a[7])
+
+        #print line
+        f.write(line+'\n')
+
+    f.close()
