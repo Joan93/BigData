6da39f3679d068821d9ad0f1c7f3a66c30b486d6
AnaCristinaHernandez <anacristina9111@gmail.com>
2016-04-26 10:57:08 -0700

    Equation fixed

--- GetTrafficMatrix_Distance1.py
+++ GetTrafficMatrix_Distance1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from collections import OrderedDict
 #import resource
 
@@ -22,10 +23,10 @@ with open("Process_Data/RDD/Prematrix_data_python.txt","r") as fid:
             dLon = (lon2 - lon1) * (Mathf.PI / 180)
             lat1 = lat1 * (Mathf.PI / 180)
             lat2 = lat2 * (Mathf.PI / 180)
-            a = Mathf.Sin(dLat / 2) * Mathf.Sin(dLat / 2) + Mathf.Sin(dLon / 2) * Mathf.Sin(dLon / 2) * Mathf.Cos(
-                lat1) * Mathf.Cos(lat2);
-            c = 2 * Mathf.Atan2(Mathf.Sqrt(a), Mathf.Sqrt(1 - a));
-            d = R * c;
+            a = math.asin(dLat / 2) * math.asin(dLat / 2) + math.asin(dLon / 2) * math.asin(dLon / 2) * math.acos(
+                lat1) * math.acos(lat2)
+            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+            d = R * c
 
 np.savetxt('Process_Data/RDD/TrafficMatrix_data_python.txt', Matrix, delimiter=' ',newline='\n',fmt='%i')
 
