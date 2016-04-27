d66c169496cdf81c0cedac8029b1ebe1f242965a
lucia <lucia.medina.huerta@gmail.com>
2016-04-27 14:53:31 +0200

    distance and height matrix all-to-all

--- DistanceMatrix_data_python.py
+++ DistanceMatrix_data_python.py
@@ -4,23 +4,25 @@ import os
 import math
 
 #function to calculate distance
-def Cal_distance( lon1,  lat1, height1,  lon2,  lat2, height2):
-    R = 6371 # radius of earth in km
+def Cal_distance( lon1,  lat1,   lon2,  lat2):
+    R = 6371000# radius of earth in km
     dLat = (lat2-lat1)*(math.pi / 180)
     dLon = (lon2-lon1)*(math.pi / 180)
     lat1 = lat1*(math.pi / 180)
     lat2 = lat2*(math.pi / 180)
     a = math.sin(dLat/2) * math.sin(dLat/2) + math.sin(dLon/2) * math.sin(dLon/2) * math.cos(lat1) * math.cos(lat2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    distace_base = R * c
+    return R * c
 
-    #tringle of distances
-    dHeight=math.fabs(height1-height2)             #variacion de alturas
-    d =math.sqrt(math.pow(distace_base,2)+math.pow(dHeight,2))
-    return d*1000 #distance in m
+#function to calculate distance
+def Cal_Height( height1, height2):
+    difference=height1-height2
+    return difference
+
 
 number_stations=465
 matrix=np.zeros((number_stations, number_stations))
+matrix2=np.zeros((number_stations, number_stations))
 neighbours_dic={}
 i=0
 
@@ -37,16 +39,19 @@ with open("Process_Data/RDD/Prematrix_data_python.txt", "r") as fid:
         i+=1
 print(neighbours_dic)
 
+#distance matrix
 for n in neighbours_dic:
     ne=n+1
-    for ne in neighbours_dic[n]:
-        #result = Cal_distance(2.18, 41.39,10,2.19,41.49,20)
-        #print(neighbours_dic.values()[1][0])
-        result = Cal_distance(neighbours_dic.values()[n][0], neighbours_dic.values()[n][1],neighbours_dic.values()[n][2],neighbours_dic.values()[ne][0],neighbours_dic.values()[ne][1],neighbours_dic.values()[ne][2] )
+    for ne in range(ne,len(neighbours_dic)):
+        result = Cal_distance(neighbours_dic.values()[n][0], neighbours_dic.values()[n][1],neighbours_dic.values()[ne][0],neighbours_dic.values()[ne][1])
         matrix[n,ne]=result
         matrix[ne,n]=result
+        result2 = Cal_Height(neighbours_dic.values()[n][2],neighbours_dic.values()[ne][2])
+        matrix2[n,ne]=result2
+        matrix2[ne,n]=result2
 
 np.savetxt('Process_Data/RDD/AlldistanceMatrix_data_python.txt', matrix, delimiter=' ',newline='\n',fmt='%i')
+np.savetxt('Process_Data/RDD/AllheightMatrix_data_python.txt', matrix2, delimiter=' ',newline='\n',fmt='%i')
 
 '''
 for n in neighbours_tup:
