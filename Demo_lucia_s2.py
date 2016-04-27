94b00d6a26b4f57dbfe463730f691eff8d7108cf
lucia <lucia.medina.huerta@gmail.com>
2016-04-27 13:22:16 +0200

    session 2 lucia: distance between ALL stations

--- Demo_lucia_s2.py
+++ Demo_lucia_s2.py
@@ -19,7 +19,8 @@ with open("Process_Data/RDD/Prematrix_data_python.txt", "r") as fid:
         for element in all_partners:
             matrix[int(id)-1,int(element)-1]=1
             matrix[int(element)-1,int(id)-1]=1
-print(altitude)np.savetxt('Process_Data/RDD/TrafficMatrix_data_python.txt', matrix, delimiter=' ',newline='\n',fmt='%i')#
+print(altitude)
+np.savetxt('Process_Data/RDD/TrafficMatrix_data_python.txt', matrix, delimiter=' ',newline='\n',fmt='%i')#
 
 print(np.argmin(altitude)) #minimum altitude: 11m
 print(np.argmax(altitude)) #maximum altitude: 331m
--- DistanceMatrix_data_python.py
+++ DistanceMatrix_data_python.py
@@ -1,54 +1,61 @@
 import numpy as np
 from collections import OrderedDict
 import os
+import math
 
-number_stations=496
-pi=3.14159265358979323846
-radius=6371 #radius of earth in km
+#function to calculate distance
+def Cal_distance( lon1,  lat1, height1,  lon2,  lat2, height2):
+    R = 6371 # radius of earth in km
+    dLat = (lat2-lat1)*(math.pi / 180)
+    dLon = (lon2-lon1)*(math.pi / 180)
+    lat1 = lat1*(math.pi / 180)
+    lat2 = lat2*(math.pi / 180)
+    a = math.sin(dLat/2) * math.sin(dLat/2) + math.sin(dLon/2) * math.sin(dLon/2) * math.cos(lat1) * math.cos(lat2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    distace_base = R * c
 
+    #tringle of distances
+    dHeight=math.fabs(height1-height2)             #variacion de alturas
+    d =math.sqrt(math.pow(distace_base,2)+math.pow(dHeight,2))
+    return d*1000 #distance in m
+
+number_stations=465
 matrix=np.zeros((number_stations, number_stations))
-altitude_arr=([])
-longitude_arr=([])
-latitude_arr=([])
-neighbours_tup={}
+neighbours_dic={}
 i=0
-n=0
+
 with open("Process_Data/RDD/Prematrix_data_python.txt", "r") as fid:
     for line in fid:
         f=line.split(';')
         id=f[0]
-        altitude=f[2]
+        height=f[2]
         latitude=f[3]
         longitude=f[4]
         partners=f[5]
         all_partners=partners.split(',')
-        neighbours_tup[i]=(float(latitude), float(longitude), all_partners)
+        neighbours_dic[i]=(float(longitude), float(latitude), int(height))
         i+=1
+print(neighbours_dic)
 
-print(neighbours_tup)
+for n in neighbours_dic:
+    ne=n+1
+    for ne in neighbours_dic[n]:
+        #result = Cal_distance(2.18, 41.39,10,2.19,41.49,20)
+        #print(neighbours_dic.values()[1][0])
+        result = Cal_distance(neighbours_dic.values()[n][0], neighbours_dic.values()[n][1],neighbours_dic.values()[n][2],neighbours_dic.values()[ne][0],neighbours_dic.values()[ne][1],neighbours_dic.values()[ne][2] )
+        matrix[n,ne]=result
+        matrix[ne,n]=result
 
+np.savetxt('Process_Data/RDD/AlldistanceMatrix_data_python.txt', matrix, delimiter=' ',newline='\n',fmt='%i')
+
+'''
 for n in neighbours_tup:
     for ne in neighbours_tup[n][2]:
         n_ne=(int(ne))
-        print(neighbours_tup[n][0]-neighbours_tup[n_ne][0])
-
-
-
-
-
-
-        #altitude_arr=np.append(altitude_arr, altitude)
-        #latitude_arr=np.append(latitude_arr, latitude)
-        #longitude_arr=np.append(longitude_arr, longitude)
-
-
-
-'''for i in stations:
-    latitude()
-
-
-
-        for element in all_partners:
-            matrix[int(id)-1,int(element)-1]=
-            matrix[int(element)-1,int(id)-1]=1
-        '''
+        #print(neighbours_tup[n][0]-neighbours_tup[n_ne][0])
+        print(n_ne)
+        dlat=((neighbours_tup[n][0]-neighbours_tup[n_ne][0])*(pi/180))
+        dlon=((neighbours_tup[n][1]-neighbours_tup[n_ne][1])*(pi/180))
+        lat=(neighbours_tup[n][0]*(pi/180))
+        lon=(neighbours_tup[n][1]*(pi/180))
+'''
