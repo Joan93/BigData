8661df80872a75ec6f8723843952764992011090
Joan93 <jfloritmir@gmail.com>
2016-05-03 19:21:11 +0200

    estadistica ejemplo con: mean, variance, nonzeros, max, min correlación

--- statistics.py
+++ statistics.py
@@ -12,7 +12,7 @@ import sys
 if ("exec" not in sys.argv):
  #Autoexecute SDK
  import os
- os.system('/Users/JoanIgnasi/Documents/spark-1.6.1-bin-hadoop2.6/bin/spark-submit statistics.py exec')
+ os.system('/Users/JoanIgnasi/Documents/spark-1.3.0-bin-hadoop2.4/bin/spark-submit statistics.py exec')
 
 
 # Programa
@@ -36,6 +36,11 @@ else:
  rows = sc.parallelize([v0, v1, v2])
 
  summary = Statistics.colStats(rows)
+
+
+
+# estadisticas
+
  print(summary.mean())
  print(summary.variance())
  print(summary.numNonzeros())
@@ -43,24 +48,11 @@ else:
  print (summary.min())
 
 
- '''
-
- rdd = sc.parallelize([range(0, 10), range(10,20), range(20, 30)])
-
-
- #mat = ([2, 0, 0, -2])
-
-# print rdd.mean()
- xs = (("x1", 1), ("x2", 2), ("x3", 3))
- datos=zip(*xs)
- #datos = np.array([7,8,1,3])
- mat = sc.parallelize(datos)
 
 
 
- #summary = Statistics.colStats(rdd)
+# correlacion
 
- print(dep.mean())
- print(summary.variance())
- print(summary.numNonzeros())
-  '''
+
+ print(Statistics.corr(rows, method="pearson"))
+
