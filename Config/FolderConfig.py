27d5ace99e3ff55e0443317e8293048c54d9373f
rodrigo.sampedro <ropnom5291@gmail.com>
2016-05-10 20:04:57 +0200

    ading config folder...

--- Config/FolderConfig.py
+++ Config/FolderConfig.py
@@ -1,3 +1,5 @@
 
 data_folder = "Data/"
 data_process_folder = "Process_Data/RDD/"
+data_process_folder_superfile = "Process_Data/RDD/SuperFile/"
+data_process_folder_station = "Process_Data/RDD/Station/"
